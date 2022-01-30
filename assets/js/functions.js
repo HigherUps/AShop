8ba84d9af5ec4d24b36f254031c1534e764150a9
TheHigherUps <thehigherupsclassified@gmail.com>
2022-01-29 21:29:44 -0500

    Add files via upload

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,8 +3,14 @@ function beans() {
     console.log('%c WARNING!', 'background-color: #cc3300; color: white; padding-right: 25px; font-size: 50px;')
     console.log('%c Pasting or Entering Anything Into The Console May Put Your Account At Risk', 'color: white; background-color: #cc3300; font-size: 20px;')
 }
-function sdsd() {
+function CCMoJ() {
     var aud = document.getElementById("auii");
+    aud.remove();
+}
+function sdsd() {
+    var btn = document.querySelector("div.EaLpb");
+    var aud = document.getElementById("auii");
+    btn.style.display = "grid"
     aud.play();
     aud.muted = false;
 }
