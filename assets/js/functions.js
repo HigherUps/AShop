4fa7a5039b8c3ae196ab3297e564cb864a3f9ffe
TheHigherUps <thehigherupsclassified@gmail.com>
2022-02-04 17:03:59 -0500

    Add files via upload

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,19 +1,39 @@
 let pvlvm = 0;
+document.addEventListener("click", sdsd)
+document.getElementById("cwcyW").addEventListener("click", musicOff);
+document.getElementById("YyfJm").addEventListener("click", musicOn)
 function beans() {
-    console.log('%c TheHigherUps Classified 2022', 'color:red; font-size: 30px; background-color: white;')
-    console.log('%c WARNING!', 'background-color: #cc3300; color: white; padding-right: 25px; font-size: 50px;')
-    console.log('%c Pasting or Entering Anything Into The Console May Put Your Account At Risk', 'color: white; background-color: #cc3300; font-size: 20px;')
+    console.log('%c TheHigherUps Classified 2022', 'color:red; font-size: 30px; background-color: white;');
+    console.log('%c WARNING!', 'background-color: #cc3300; color: white; padding-right: 25px; font-size: 50px;');
+    console.log('%c Pasting or Entering Anything Into The Console May Put Your Account At Risk', 'color: white; background-color: #cc3300; font-size: 20px;');
 }
-function CCMoJ() {
-    var aud = document.getElementById("auii");
-    aud.remove();
+function musicOn() {
+    audio = document.getElementById("auii");
+    buttonOff = document.getElementById("cwcyW");
+    buttonOn = document.getElementById("YyfJm");
+    buttonOn.style.display = "none";
+    buttonOff.style.display = "grid";
+    audio.muted = false;
 }
+function musicOff() {
+    audio = document.getElementById("auii");
+    buttonOff = document.getElementById("cwcyW");
+    buttonOn = document.getElementById("YyfJm");
+    buttonOn.style.display = "grid";
+    buttonOff.style.display = "none";
+    audio.muted = true;
+}
+// function CCMoJ() {
+//     var aud = document.getElementById("auii");
+//     var btn = document.getElementById("jyFfS");
+//     aud.pause();
+//     btn.remove();
+// }
 function sdsd() {
     var btn = document.querySelector("div.EaLpb");
     var aud = document.getElementById("auii");
-    btn.style.display = "grid"
+    btn.style.display = "grid";
     aud.play();
-    aud.muted = false;
 }
 function cdNl() {
     document.getElementById("ZzNmm").style.display = "none";
@@ -34,7 +54,8 @@ function ayZu() {
     var el = document.getElementById("ZILb").value;
     var audio = new Audio("/assets/sounds/buzzer.mp3");
     if(el == '') {
-        console.log("ErrCode-InputZILb Value equals none")
+        console.log("ErrCode-InputZILb Value equals none");
+        alert("You must enter text before submitting");
     }else {
         $('#woah').delay(100).show(10);
         audio.play(); 
@@ -45,7 +66,8 @@ function vKacI() {
     var bb = document.getElementById("bmPpJ").value;
     var audio = new Audio("/assets/sounds/buzzer.mp3");
     if(bb == '') {
-        console.log("ErrCode-InputZILb Value equals none")
+        console.log("ErrCode-InputZILb Value equals none");
+        alert("You must enter text before submitting");
     }else {
         $('#woah').delay(100).show(10);
         audio.play(); 
